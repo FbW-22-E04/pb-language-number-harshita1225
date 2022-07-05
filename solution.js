56b9d9fc662a09a346e5baaaccbd99605cb7dad8
harshita <harshita1225@gmail.com>
2022-07-05 12:23:10 +0200

    task completed

---
+++ solution.js
@@ -0,0 +1,14 @@
+let introSentence = "hello, my name is Fran and I am";
+let age =  25;
+console.log(introSentence + ' ' + age);
+
+// + operator is used to merge 2 strings
+
+let integer= '1005';
+const ConvertNumber = integer*1;
+console.log('this is a number',ConvertNumber);
+
+let float = "10.05";
+
+console.log(~~float);
+console.log(Math.floor(float));
